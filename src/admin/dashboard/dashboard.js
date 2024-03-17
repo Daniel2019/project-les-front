1840d00a96689a2c39e54cc006ef5c12c88423b8
Daniel2019 <Daniel_dandico@outlook.com>
2024-03-17 18:32:12 -0300

    feat: desenvolvimento das telas de visualizacao

--- src/admin/dashboard/dashboard.js
+++ src/admin/dashboard/dashboard.js
@@ -1,3 +1,26 @@
 function logout() {
     window.location.href = "/project-les-front/src";
 }
+
+function showChart(){
+    const ctx = document.getElementById('myChart');
+    
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+}
