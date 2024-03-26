5198ab8c1f79532b20accff5d0758108fa64dce5
Daniel2019 <Daniel_dandico@outlook.com>
2024-03-25 23:10:52 -0300

    feat: carregando dados em meus dados

--- src/usuario/painel/meus_dados/meus_dados.js
+++ src/usuario/painel/meus_dados/meus_dados.js
@@ -5,7 +5,7 @@ $().ready(function () {
 // FUNÇÕES DE INTEGRAÇÃO COM O BACK-END
 function consultarMeusDados(){
 
-    const url = "http://localhost:8080/clientes/listar";
+    const url = "http://localhost:8080/clientes/listar/1";
 
     fetch(url, {
         method: 'GET'
@@ -17,29 +17,38 @@ function consultarMeusDados(){
             return;
         }
 
-        data.map((cliente) => {
-            $("#nome").val(1);
-            $("#sobrenome").val(1);
-            $("#email").val(1);
-            $("#dataNascimento").val(1);
-            $("#cpf").val(1);
-            $("#genero").val(1);
-            $("#senha").val(1);
-            $("#tipoContato").val(1);
-            $("#ddd").val(1);
-            $("#telefone").val(1);
-            $("#nomeEndereco").val(1);
-            $("#tipoResidencia").val(1);
-            $("#cep").val(1);
-            $("#logradouro").val(1);
-            $("#tipoLogradouro").val(1);
-            $("#numero").val(1);
-            $("#bairro").val(1);
-            $("#cidade").val(1);
-            $("#estado").val(1);
-            $("#pais").val(1);
-            $("#observacoes").val(1);
-        })
+        const cliente = data;
+
+        $("#id").val(cliente.id);
+        $("#nome").val(cliente.nome);
+        $("#sobrenome").val(cliente.sobrenome);
+        $("#email").val(cliente.email);
+        $("#dataNascimento").val(1);
+        $("#cpf").val(cliente.cpf);
+        $("#genero").val(1);
+        $("#senha").val(cliente.senha);
+        $("#tipoContato").val(1);
+        $("#ddd").val();
+        $("#telefone").val(1);
+
+        cliente.enderecosEntrega.map((endereco) => {
+
+            if(endereco.tipoEndereco == "RESIDENCIAL"){
+                $("#nomeEndereco").val(endereco.nomeEndereco);
+                $("#tipoResidencia").val(1);
+                $("#cep").val(endereco.cep);
+                $("#logradouro").val(endereco.logradouro);
+                $("#tipoLogradouro").val(1);
+                $("#numero").val(endereco.numero);
+                $("#bairro").val(endereco.bairro);
+                $("#cidade").val(endereco.cidade.nome);
+                $("#estado").val(endereco.cidade.estado.nome);
+                $("#pais").val(endereco.cidade.estado.pais);
+                $("#observacoes").val(endereco.observacoes);
+            }
+
+        });
+
     }).catch(error => {
         $("#nome").val(1);
         $("#sobrenome").val(1);
